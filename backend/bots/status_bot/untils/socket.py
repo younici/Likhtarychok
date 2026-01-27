bab970c76529a5b55d0bb58df18ca300ad73fc51
younici <97344998+younici@users.noreply.github.com>
2026-01-27 20:46:09 +0200

    Implement debounce logic for light ON/OFF events in WebSocket connection handling

--- backend/bots/status_bot/untils/socket.py
+++ backend/bots/status_bot/untils/socket.py
@@ -11,39 +11,57 @@ _start: float = 0
 
 _last_con_time: float = 0
 _last_con_duration: float = 0
-
+_last_on_trigger: float = 0
+_last_off_trigger: float = 0
+DEBOUNCE_ON_SEC = 30
+DEBOUNCE_OFF_SEC = 30
 async def handle_connection(websocket):
     _log.info("New WebSocket connection established")
-    global _status
-    global _start
-    global _last_con_duration
-    global _last_con_time
+
+    global _status, _start
+    global _last_con_duration, _last_con_time
+    global _last_on_trigger, _last_off_trigger
+
+    now = time.time()
 
     if _start == 0:
-        _start = time.time()
+        _start = now
 
+    # ===== DEBOUNCE ON =====
     try:
         if Light_Events.on and not _status:
-            _log.info("Triggering light ON event")
-            _status = True
-            await Light_Events.on()            
+            if now - _last_on_trigger >= DEBOUNCE_ON_SEC:
+                _log.info("Triggering light ON event")
+                _last_on_trigger = now
+                _status = True
+                await Light_Events.on()
+            else:
+                _log.debug("ON ignored (debounce)")
     except Exception as e:
         _log.error(f"Error executing Light_Events.on: {e}")
+
     try:
         await websocket.wait_closed()
     finally:
         if not states.closing:
+            now = time.time()
+
+            # ===== DEBOUNCE OFF =====
             try:
                 if Light_Events.off and _status:
-                    _last_con_duration = get_connection_time()
-                    _last_con_time = time.time()
+                    if now - _last_off_trigger >= DEBOUNCE_OFF_SEC:
+                        _last_con_duration = get_connection_time()
+                        _last_con_time = now
 
-                    _log.info("Triggering light OFF event")
+                        _log.info("Triggering light OFF event")
+                        _last_off_trigger = now
 
-                    await Light_Events.off()
+                        await Light_Events.off()
 
-                    _start = 0
-                    _status = False
+                        _start = 0
+                        _status = False
+                    else:
+                        _log.debug("OFF ignored (debounce)")
             except Exception as e:
                 _log.error(f"Error executing Light_Events.off: {e}")
 
