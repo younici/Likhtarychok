862da084c422068d16134a9e217ba5637ee0e5bc
younici <kostantinreksa@gmail.com>
2026-09-20 00:33:45 +0300

    fixed models

--- backend/db/orm/models/__init__.py
+++ backend/db/orm/models/__init__.py
@@ -2,7 +2,6 @@ from .user import User
 from .subscription import Subscription
 from .tg_sub import TgSub
 from .support import SupportAdmin, SupportBan, SupportTicket, SupportTicketMessage
-from .simple_model import Subs
 
 __all__ = [
     "User",
