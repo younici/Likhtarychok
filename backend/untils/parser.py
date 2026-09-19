c8831c2f14148b3f35eb69bbe66bf29eac4ee071
younici <kostantinreksa@gmail.com>
2026-09-20 01:19:09 +0300

    on err fix

--- backend/untils/parser.py
+++ backend/untils/parser.py
@@ -14,26 +14,26 @@ log = logging.getLogger(__name__)
 log.info(f"cache status: {CAN_CACHE}")
 
 async def parse(queue: int):
-    queue = tools.queue_to_index(queue)
-    bias = tools.bias_from_index(queue)
-
-    text = ""
-
-    if CAN_CACHE:
-        text = await cache.get_cache(queue)
-        log.info("cache used")
-    else:
-        text = await tools.get_status(queue, bias)
-
-    if text is None:
-        return None
-
-    status = []
-    colors = []
-
-    html = BeautifulSoup(text, "html.parser")
-
     try:
+        queue = tools.queue_to_index(queue)
+        bias = tools.bias_from_index(queue)
+
+        text = ""
+
+        if CAN_CACHE:
+            text = await cache.get_cache(queue)
+            log.info("cache used")
+        else:
+            text = await tools.get_status(queue, bias)
+
+        if text is None:
+            return None
+
+        status = []
+        colors = []
+
+        html = BeautifulSoup(text, "html.parser")
+
         for i in html:
             style = i.get("style", "")
             color = None
